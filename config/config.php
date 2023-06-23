e6cc3f183d46c21eb0cf161bd446ac079525bffb
Maarten Thiebou <getappkit@gmail.com>
2023-06-23 09:05:07 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -9,7 +9,6 @@ return [
         'debug' => true,
         'db' => [
             'driver'   => 'sqlite',
-            'type'     => 'sqlite',
             'database' => BASE_DIR .'/database/data.sqlite', #full path to file
         ],
     ],
